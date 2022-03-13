670c7b191aa27e4803739651e1ff675a9982c25c
mlixjf <1002034125@qq.com>
2022-03-13 18:10:46 +0800

    第四章，数据库

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 """
 os.environ是环境变量字典
@@ -6,6 +7,17 @@ os.getenv是一个函数
 注：使用flask-wtf需要首先配置SECRET_KEY属性, CSRF需要密钥
 """
 
+basedir = Path(__file__).parent
+
 
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
+    SQLALCHEMY_DATABASE_URI = (os.environ.get("DATABASE_URL") or
+                               "sqlite:///" + os.path.join(basedir, 'app.db') +
+                               "?" +
+                               "check_same_thread=False")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+if __name__ == '__main__':
+    print(Config.SQLALCHEMY_DATABASE_URI)
