d43c71e8b6ae15a9b7d039ab779a7f60078fd003
mlixjf <1002034125@qq.com>
2022-03-19 22:33:30 +0800

    第八章：粉丝

--- app/models.py
+++ app/models.py
@@ -5,6 +5,11 @@ from app import db, login
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import UserMixin
 
+followers = db.Table("followers",
+        db.Column("follower_id", db.Integer, db.ForeignKey("user.id")),
+        db.Column("followed_id", db.Integer, db.ForeignKey("user.id"))
+    )
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,6 +19,12 @@ class User(db.Model, UserMixin):
     posts = db.relationship("Post", backref="author", lazy="dynamic")
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow())
+    followed = db.relationship(
+        "User", secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref("followers", lazy="dynamic"), lazy="dynamic"
+    )
 
     def set_password(self, password: str) -> None:
         self.password_hash = generate_password_hash(password)
@@ -26,6 +37,26 @@ class User(db.Model, UserMixin):
         digest = md5(self.email.lower().encode("utf-8")).hexdigest()
         return (f"https://www.gravatar.com/avatar/{digest}?"
                 f"d=identicon&s={size}")
+
+    def is_following(self, user):
+        return (self.followed.filter(followers.c.followed_id == user.id)
+                .count() > 0)
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+            followers.c.follower_id == self.id)
+
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
 
     def __repr__(self):
         return f"<User {self.username}>"
@@ -44,4 +75,5 @@ class Post(db.Model):
 @login.user_loader
 def load_user(id: str) -> User:
     return User.query.get(int(id))
+
 
